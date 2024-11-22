db20e6ba8ccd44eed5d9f4cb88802c2e7f1d74a9
Pablo9924 <pnf@aluno.ifnmg.edu.br>
2024-11-22 22:02:49 +0000

    Dado

--- bd.js
+++ bd.js
@@ -1,23 +1,19 @@
 //bd.js
 import pkg from "pg";
-
 const { Pool } = pkg;
 
-async function selectUsuarios() {
-  const client = await coneccao();
-  const res = await client.query("SELECT * FROM usuario");
-  client.release();
-  return res.rows;
-}
-
-async function coneccao() {
+async function connect() {
   const pool = new Pool({
     connectionString: process.env.URL_BD,
   });
-  return pool.conneccao();
+  return pool.connect();
 }
 
-//bd.js
+async function selectUsuarios() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM usuario");
+	  client.release();
+  return res.rows;
+}
 
 export { selectUsuarios };
-
